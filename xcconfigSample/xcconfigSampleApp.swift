bd1a9b3b2082c619b7a2114916b68d2ca9453bdc
satorun <satorun.org@gmail.com>
2026-01-30 00:33:48 +0900

    Initial Commit

---
+++ xcconfigSample/xcconfigSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcconfigSampleApp.swift
+//  xcconfigSample
+//
+//  Created by satorun on 2026/01/30.
+//
+
+import SwiftUI
+
+@main
+struct xcconfigSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcconfigSampleTests/xcconfigSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  xcconfigSampleTests.swift
+//  xcconfigSampleTests
+//
+//  Created by satorun on 2026/01/30.
+//
+
+import Testing
+@testable import xcconfigSample
+
+struct xcconfigSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
